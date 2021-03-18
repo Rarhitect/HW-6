99748c6f22c130698ecca4a03dbab8cf320f1e3e
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-18 23:54:59 +0300

    initial

--- HW6-4/main.cpp
+++ HW6-4/main.cpp
@@ -2,13 +2,14 @@
 //  main.cpp
 //  HW6-4
 //
-//  Created by Alyaev Roman on 18.03.2021.
+//  Created by Alyaev Roman on 17.03.2021.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    
+    
     return 0;
 }
