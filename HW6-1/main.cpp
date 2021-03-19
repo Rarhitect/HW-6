2a28a42f906711ac1885186c208c108b587ef5a7
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-20 01:01:33 +0300

    Набросок задачи, не хватает реализации предиката

--- HW6-1/main.cpp
+++ HW6-1/main.cpp
@@ -9,23 +9,19 @@
 #include <fstream>
 #include <string>
 
-bool is_comment(char char1, char char2)
+bool is_comment()
 {
-    static int counter = 0;
-    if(char2 == '"' and char1 != '//')
-        {
-            counter ? --counter : ++counter;
-        }
-    if(char2 == '//' and char1 != '//' and !counter)
-        {
-            return true;
-        }
+    // the test is it a comment or not
+    
     return false;
 }
 
 int main(int argc, const char * argv[])
 {
-    std::string filename = "code.cpp";
+    std::string filename;
+    std::cout << "Enter the filename (name.extention): ";
+    std::cin >> filename;
+    
     std::fstream inout(filename, std::ios::in | std::ios::out);
     
     if(!inout)
@@ -34,20 +30,12 @@ int main(int argc, const char * argv[])
         exit(1);
     }
     
-    char char1 = ' '; char char2;
-    
     while(inout)
     {
-        inout.get(char2);
-        
-        if(is_comment(char1, char2))
+        if(is_comment() == true)
         {
-            inout.seekg(-1, std::ios::cur);
-            inout.put(' ');
-            inout.put(' ');
+            // deleting the symbols that create comments
         }
-        
-        char1 = char2;
     }
     
     return 0;
