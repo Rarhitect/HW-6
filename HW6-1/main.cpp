537f7b18dd33add872862fbe41afaf5addb7886c
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-20 01:08:17 +0300

    Набросок задачи, не хватает только предиката

--- HW6-1/main.cpp
+++ HW6-1/main.cpp
@@ -2,13 +2,41 @@
 //  main.cpp
 //  HW6-1
 //
-//  Created by Alyaev Roman on 20.03.2021.
+//  Created by Alyaev Roman on 17.03.2021.
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+bool is_comment()
+{
+    // the test is it a comment or not
+    
+    return false;
+}
+
+int main(int argc, const char * argv[])
+{
+    std::string filename;
+    std::cout << "Enter the filename (name.extention): ";
+    std::cin >> filename;
+    
+    std::fstream inout(filename, std::ios::in | std::ios::out);
+    
+    if(!inout)
+    {
+        std::cerr << "Can't open/write..." << std::endl;
+        exit(1);
+    }
+    
+    while(inout)
+    {
+        if(is_comment() == true)
+        {
+            // deleting the symbols that create comments
+        }
+    }
+    
     return 0;
 }
