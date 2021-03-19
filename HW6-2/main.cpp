5e56ebe0c6d7bac7561c173614fd4b2d99e647fb
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-19 20:46:01 +0300

    Есть один баг, который надо пофиксить (файл создается корректно, но вывод строк какой-то косячный)

--- HW6-2/main.cpp
+++ HW6-2/main.cpp
@@ -6,10 +6,54 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int argc, const char * argv[])
 {
+    std::string filename = "Strings.txt";
+    constexpr int string_length = 13;
+    constexpr int string_quantity = 4;
+    std::ofstream o_f(filename);
     
+    if(!o_f)
+    {
+        std::cerr << "Can't open..." << std::endl;
+        exit(1);
+    }
+    
+    int string_number;
+    std::cout << "Insert string number: ";
+    std::cin >> string_number;
+    
+    if((string_number > string_quantity) and (string_number < 0))
+    {
+        std::cout << "File doesn't have that number" << std::endl;
+        exit(1);
+    }
+    
+    static char filling_character = 'A';
+    
+    for(auto i = 0; i < string_quantity; i++)
+    {
+        for(auto j = 0; j < string_length; j++)
+        {
+            o_f << filling_character;
+            ++filling_character;
+            if(filling_character == '[')
+            {
+                filling_character += 6;
+            }
+        }
+        o_f << std::endl;
+    }
 
+    std::ifstream i_f(filename);
+    i_f.seekg(string_number, std::ios::beg);
+    
+    std::string destination_string;
+    std::getline(i_f, destination_string);
+    std::cout << "Your destination string is:" << std::endl << destination_string << std::endl;
+    
     return 0;
 }
